c91b8503e4e02d00c7f6707dd61990e4cf08b976
bekdev01 <bekzod.m070@gmail.com>
2022-06-09 12:02:23 +0500

    tugadi

--- src/Pages/PicturePage/pic.js
+++ src/Pages/PicturePage/pic.js
@@ -7,7 +7,8 @@ import img1 from '../../images/pic/img1.jpeg'
 import img2 from '../../images/pic/img2.jpg'
 import img3 from '../../images/pic/img3.jpg'
 import img4 from '../../images/pic/img4.jpg'
-import {ImageList, ImageListItem} from "@mui/material";
+import {Card, CardActionArea, CardContent, CardMedia, ImageList, ImageListItem, Typography} from "@mui/material";
+import {NavLink} from "react-router-dom";
 
 const Item = styled(Box)(({theme}) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -39,23 +40,46 @@ const itemData = [
 
 export default function BasicGrid() {
     return (
-        <ImageList sx={{width: 1200, height: 500}} cols={4} rowHeight={400} className='mb-0'>
-            {
-                itemData.map((item, index) => (
-                    <Grid item xs={12} sm={6} md={3} key={index}>
-                        <Item>
-                            <ImageListItem key={item.img} className='mx-2'>
-                                <img
-                                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                    alt={item.title}
-                                    loading="lazy"
-                                />
-                            </ImageListItem>
-                        </Item>
-                    </Grid>
-                ))
-            }
-        </ImageList>
+        // <ImageList sx={{width: 1200, height: 500}} cols={4} rowHeight={400} className='mb-0'>
+        //     {
+        //         itemData.map((item, index) => (
+        //             <Grid item xs={12} sm={6} md={3} key={index}>
+        //                 <Item>
+        //                     <ImageListItem key={item.img} className='mx-2'>
+        //                         <img
+        //                             src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+        //                             srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+        //                             alt={item.title}
+        //                             loading="lazy"
+        //                         />
+        //                     </ImageListItem>
+        //                 </Item>
+        //             </Grid>
+        //         ))
+        //     }
+        // </ImageList>
+        <Box sx={{flexGrow: 1}}>
+            <Grid container spacing={2}>
+                {
+                    itemData.map((i, index) => {
+                            return (
+                                <Grid item xs={12} sm={6} md={3} key={index}>
+                                    <Item>
+                                        <CardActionArea className={'border rounded rounded-1'}>
+                                            <Card sx={{maxWidth: 345, maxHeight: 300, minHeight: 200}}>
+                                                <CardMedia
+                                                    component="img"
+                                                    height="300"
+                                                    image={i.img}
+                                                    alt="Ops! Something went wrong"
+                                                />
+                                            </Card>
+                                        </CardActionArea>
+                                    </Item>
+                                </Grid>
+                            );
+                    })}
+            </Grid>
+        </Box>
     );
 }
